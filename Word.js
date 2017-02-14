a831dd66ff63cdd7b79d0cb724857df73eadb2cb
John Krug <john@Johns-MacBook-Pro.local>
2017-02-14 18:15:21 -0500

    WOrking Game

--- Word.js
+++ Word.js
@@ -1,33 +1,34 @@
  // * `Word.js` should contain all of the methods which will 
  // check the letters guessed versus the random word selected.
-var letter = require("./letter.js");
+
 
 var Word = function(word) {
 	this.word = word;
-	this.lettersArray = [];
-	console.log("Here: " + word);
-	for (var i = 0; i < this.word.length; i++) {
-		//pass in each letter to letter.js and get letter(object)
-		this.lettersArray.push(new letter(this.word[i]));
-	}
-	console.log(this.lettersArray);
-		// console.log(lettersArray[0].);
-	for (var l = 0; l < this.lettersArray.length; l++) {
-		var currentLetter = this.lettersArray[l];
-		console.log(currentLetter.show());
-	}
-}
-Word.prototype.display = function() {
-	this.string = "";
 
-	for(var i = 0; i < this.lettersArray.length; i++) {
-		this.string += lettersArray[i].letter;
-		console.log(string);
-		return this.string;
-		
+	this.check = function(letter, array) {
+		var found = false;
+		for (var i = 0; i < array.length; i++) {
+			if (letter === array[i].letter) {
+				array[i].found = true;
+				found = true;
+			};
+		};
 
-	}
-	return this.string;
-	console.log(this.string);
-}
+		if (found == true) {
+			return true;
+		} else { 
+			return false;
+		};
+	};
+
+	this.isSolved = function(array) {
+		for (var i = 0; i < array.length; i++) {
+			if (array[i].found == false) {
+				return false;
+			};
+		};
+		return true;
+	};
+};
+
 module.exports = Word;
--- game.js
+++ game.js
@@ -1,15 +1,94 @@
 var inquirer = require("inquirer");
-var word = require("./word.js");
+var Word = require("./Word.js");
+var Letter = require("./Letter.js");
 var colors = require("colors");
 
-var wordArray = ["SHIVA", "ZUES", "MINOTAUR", "MEDUSA", "AMATERASU",
-				"KYUUBI", "ISIS", "ANUBIS", "KALI"];
+var wordArray = ["Dark Souls", "Conan Exiles", "Overwatch", "Rainbow Six Siege", "Binding of Isaac"];
+
+// Make global var's for lives, guessarray, letter array getWord and word. than make a startgame func. Maybe????
+//In restart func. reset global vars.
+
+var getWord;
+var word;
+var lives = 10;
+var guessesArray = [];
+var lettersArray = [];
 
 var startGame = function() {
-	this.currentWord = new word(wordArray[Math.floor(Math.random() * wordArray.length)]);
+	getWord = new Word(wordArray[Math.floor(Math.random() * wordArray.length)]);
+	word = getWord.word;
+	word = word.toUpperCase();
+	console.log(word);
+	console.log("word: " + word);
+	console.log("getWord: " + getWord);
+	// var lives = 10;
 
-	this.lives = 10;
-	this.guesses = [];
+	for (var i = 0; i < word.length; i++) {
+		lettersArray.push(new Letter(word[i]));
+	};
+
+	console.log(lettersArray);
+	getGuess(getWord, lettersArray);
 }
+// var getWord = new Word(wordArray[Math.floor(Math.random() * wordArray.length)]);
+// var word = getWord.word;
+// console.log("word: " + word);
+// console.log("getWord: " + getWord);
+// var lives = 10;
+// var guessesArray = [];
+// var lettersArray = [];
 
-var testWord = word("CIGS");
+// for (var i = 0; i < word.length; i++) {
+// 	lettersArray.push(new Letter(word[i]));
+// };
+
+// console.log(lettersArray);
+
+var getGuess = function() {
+	// displayWord(lettersArray);
+	inquirer.prompt({
+		type: "input",
+		name: "guess",
+		message: "What is your guess: "
+	}).then(function(data) {
+		var letterUp = data.guess.toUpperCase();
+		guessesArray.push(letterUp);
+
+		console.log(getWord.check(data.guess.toUpperCase(), lettersArray));
+
+		if (getWord.check(data.guess.toUpperCase(), lettersArray) == true) {
+			console.log("Letter Found!");
+			displayWord(lettersArray);
+		} else {
+			lives --;
+			if (lives > 0) {
+				console.log("Wrong. Guess again!");
+				console.log(lives + " lives remaining.");
+			} else {
+				console.log("Out of lives. Game over.");
+			};
+		};
+
+		if (getWord.isSolved(lettersArray) == false) {
+			if (lives > 0) {
+				console.log("Guesses: " + guessesArray);
+				getGuess(getWord, lettersArray);
+			};
+		} else {
+			displayWord(lettersArray);
+			console.log("You guessed the word!")
+		};
+	});
+};
+
+var displayWord = function() {
+	var wordToShow = "";
+	for (var i = 0; i < lettersArray.length; i++) {
+		wordToShow += lettersArray[i].show() + " ";
+	};
+	console.log(wordToShow);
+};
+
+
+
+startGame();
